a10a8306567909e059dad2b71095aa820ec6173d
Shemul Hossain <shemulworld@gmail.com>
2014-04-17 22:31:57 +0600

    bla bla
    bla

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include "classes.h"
+#include "misc.h"
 
 int main()
 {
+
+
+    get_option();
+    //get the choice from user
+    int choice;
+    cin >> choice;
+
+    execute_op(choice);
 
 
 }
