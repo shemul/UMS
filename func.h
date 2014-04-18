2540e729490943c7d991d00fa045c5531bd8578d
Shemul Hossain <shemulworld@gmail.com>
2014-04-18 12:15:12 +0600

    added another header file for various function
    added this func.h for works with various func to make neat main.cpp

---
+++ func.h
@@ -0,0 +1,9 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+// added classes.h for all property for a project ..
+#include "classes.h"
+
+
+
+
+#endif // FUNC_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
 #include "classes.h"
-
+#include "func.h"
 int main()
 {
 
-    student shemul ;
-    shemul.set_dept_id(445);
-    shemul.set_dept_name("CSSE");
-    shemul.set_teacher_id(558);
-    shemul.set_teacher_name("Ashiq");
-    shemul.set_courses("PL2");
-    shemul.set_student_id(565);
-    shemul.set_student_name("Shemul");
-    shemul.show_student_informations();
+    show_something();
 
 }
