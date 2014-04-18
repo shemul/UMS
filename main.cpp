c7227ee4c721f612b98a17118d6dc3ea6f597950
Shemul Hossain <shemulworld@gmail.com>
2014-04-18 20:39:47 +0600

    ok ! lot of changes !
    Its time to called it v1.0

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main()
 {
     int choice ;
 
-    string str1,str2,str3 ;
-    int int1 , int2 , int3 ;
+    string str1,str2,str3,str4 ;
+    int int1 , int2 , int3,int4 ;
 
 
     show_main_options();
@@ -48,7 +48,7 @@ int main()
                     if(choice == 2)
                     {
                         fstream dept_db("./db/dept.txt");
-                        cout << "Dept ID" << "\t" << "Dept Name"<<endl;
+                        cout << "DeptID" << "\t" << "DeptName"<<endl;
                         cout <<"----------------" <<endl;
                         while (getline(dept_db,str1))
                         {
@@ -70,7 +70,7 @@ int main()
             {
                 case 1 :
                     //
-                    clear();
+                        clear();
                         cout << "Enter Department ID" <<endl;
                         cin >> int1 ;
                         cout << "Enter Department Name" << endl;
@@ -105,10 +105,10 @@ int main()
 
                     if(choice == 2)
                     {
-                        fstream dept_db("./db/teacher.txt");
+                        fstream teacher_db("./db/teacher.txt");
                         cout << "DptID " << "DptNam  " << "TeachID  "  << "TeachName  " << "Courses" <<endl;
                         cout <<"----------------------------------------------" <<endl;
-                        while (getline(dept_db,str1))
+                        while (getline(teacher_db,str1))
                         {
                             cout << str1 <<endl;
                         }
@@ -125,10 +125,54 @@ int main()
             switch(choice)
             {
                 case 1:
-                    cout << "1 ";
+                        clear();
+                        cout << "Enter Department ID" <<endl;
+                        cin >> int1 ;
+                        cout << "Enter Department Name" << endl;
+                        cin >> str1;
+                        cout << "Enter Teacher ID" << endl ;
+                        cin >> int2 ;
+                        cout << "Enter Teacher Name" << endl;
+                        cin >> str2 ;
+                        cout << "Enter Course Name" << endl;
+                        cin >> str3 ;
+                        cout << "Student ID" << endl;
+                        cin >> int4 ;
+                        cout <<"Enter Student Name" << endl;
+                        cin >> str4;
+                        if (choice == 1)
+                        {
+                            student student_ob ;
+                            student_ob.set_dept_id(int1);
+                            student_ob.set_dept_name(str1);
+                            student_ob.set_teacher_id(int2);
+                            student_ob.set_teacher_name(str2);
+                            student_ob.set_courses(str3);
+                            student_ob.set_student_id(int4);
+                            student_ob.set_student_name(str4);
+                            fstream student_db("./db/student.txt" ,fstream::in | fstream::out | fstream::app );
+                            if (!student_db)
+                            {
+                                cout << "Failed to create db" <<endl;
+                            }
+                            student_db << int1 <<"\t" << str1 <<"\t"<< int2 <<"\t"<< str2 <<"\t"<< str3 << "\t" << int4 <<"\t" <<str4 <<endl;
+                            cout << "Thank you " << endl;
+                        }
                     break;
                 case 2:
-                    cout << "2";
+                    clear();
+
+                    if(choice == 2)
+                    {
+                        fstream student_db("./db/student.txt");
+                        cout << "DptID  " << "DptNam " << "TeachID "  << "TeachName " << "Courses  " <<"StudID  " << "StudName" <<endl;
+                        cout <<"------------------------------------------------------" <<endl;
+                        while (getline(student_db,str1))
+                        {
+                            cout << str1 <<endl;
+                        }
+
+                    }
                     break;
                 default :
                     cout << "Oops 404!" << endl;
