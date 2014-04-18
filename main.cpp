63d1652fadd363693112687d43e5545b6c378677
Shemul Hossain <shemulworld@gmail.com>
2014-04-18 16:42:49 +0600

    added teacher add and view
    perfectly works

--- main.cpp
+++ main.cpp
@@ -31,41 +31,98 @@ int main()
                         dept dept_ob ;
                         dept_ob.set_dept_id(int1);
                         dept_ob.set_dept_name(str1);
-                        dept_ob.show_dept_information();
 
-                        ofstream dept_db("./db/dept.txt");
+                        fstream dept_db("./db/dept.txt" ,fstream::in | fstream::out | fstream::app );
                         if (!dept_db)
                         {
                             cout << "Failed to create db" <<endl;
                         }
-                        dept_db << int1 <<"\t" << str1;
+                        dept_db << int1 <<"\t" << str1<<endl;
+                        cout << "Thank you " << endl;
+                        system("PAUSE");
 
                     }
                     break;
                 case 2:
+                    clear();
                     if(choice == 2)
                     {
-                        ifstream dept_db("./db/dept.txt");
-                        dept_db >> str1 ;
-                        dept_db >> str2 ;
+                        fstream dept_db("./db/dept.txt");
                         cout << "Dept ID" << "\t" << "Dept Name"<<endl;
                         cout <<"----------------" <<endl;
-                        cout << str1 << "\t" << str2 ;
-                    }
+                        while (getline(dept_db,str1))
+                        {
+                            cout << str1 <<endl;
+                        }
 
+                    }
                     break;
                 default:
                     cout <<"Oops 404 !" <<endl;
+                    system("PAUSE");
             }
             break;
         case 2 :
             clear();
             show_teacher_options();
+            cin >> choice;
+            switch(choice)
+            {
+                case 1 :
+                    //
+                    clear();
+                        cout << "Enter Department ID" <<endl;
+                        cin >> int1 ;
+                        cout << "Enter Department Name" << endl;
+                        cin >> str1;
+                        cout << "Enter Teacher ID" << endl ;
+                        cin >> int2 ;
+                        cout << "Enter Teacher Name" << endl;
+                        cin >> str2 ;
+                        cout << "Teacher's Course Name" << endl;
+                        cin >> str3 ;
+                        if (choice == 1)
+                        {
+                            teacher teacher_ob ;
+                            teacher_ob.set_dept_id(int1);
+                            teacher_ob.set_dept_name(str1);
+                            teacher_ob.set_teacher_id(int2);
+                            teacher_ob.set_teacher_name(str2);
+                            teacher_ob.set_courses(str3);
+
+                            fstream teacher_db("./db/teacher.txt" ,fstream::in | fstream::out | fstream::app );
+                            if (!teacher_db)
+                            {
+                                cout << "Failed to create db" <<endl;
+                            }
+                            teacher_db << int1 <<"\t" << str1 <<"\t"<< int2 <<"\t"<< str2 <<"\t"<< str3 <<endl;
+                            cout << "Thank you " << endl;
+                        }
+                    break;
+                case 2:
+                    //
+                    clear();
+
+                    if(choice == 2)
+                    {
+                        fstream dept_db("./db/teacher.txt");
+                        cout << "DptID " << "DptNam  " << "TeachID  "  << "TeachName  " << "Courses" <<endl;
+                        cout <<"----------------------------------------------" <<endl;
+                        while (getline(dept_db,str1))
+                        {
+                            cout << str1 <<endl;
+                        }
+
+                    }
+                    break;
+
+            }
             break;
         case 3:
             clear();
             show_student_options();
             break ;
+
         default:
             clear();
             show_header();
